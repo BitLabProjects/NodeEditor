e6a6de1d1afcada291371c571a53d0d33fce8a8e
xed89 <simone.bosc@gmail.com>
2019-03-11 23:15:12 +0100

    ConnectionPath color animation

---
+++ Controls/ConnectionPath.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace NodeEditor.Controls
+{
+
+  class ConnectionPath : FrameworkElement {
+    public ConnectionPath() {
+      VisualStateManager.GoToElementState(this, "Normal", false);
+    }
+
+    private void updateVisualState() {
+      if (mIsMouseOver) {
+        if (mIsDragging) {
+          VisualStateManager.GoToElementState(this, "Drag", true);
+        } else {
+          VisualStateManager.GoToElementState(this, "MouseOver", true);
+        }
+      } else {
+        VisualStateManager.GoToElementState(this, "Normal", true);
+      }
+    }
+
+    #region Mouse handling
+    private bool mIsMouseOver;
+    private bool mIsDragging;
+    private Point mDragLastPoint;
+
+    protected override void OnMouseEnter(MouseEventArgs e) {
+      base.OnMouseEnter(e);
+      mIsMouseOver = true;
+      updateVisualState();
+    }
+    protected override void OnMouseLeave(MouseEventArgs e) {
+      base.OnMouseLeave(e);
+      mIsMouseOver = false;
+      updateVisualState();
+    }
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
+      if (CaptureMouse()) {
+        mIsDragging = true;
+        mDragLastPoint = e.GetPosition(this);
+        e.Handled = true;
+
+        updateVisualState();
+      }
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e) {
+      if (mIsDragging) {
+        var newPoint = e.GetPosition(this);
+        var delta = newPoint - mDragLastPoint;
+        mDragLastPoint = newPoint;
+
+        e.Handled = true;
+      }
+    }
+
+    protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e) {
+      if (mIsDragging) {
+        mIsDragging = false;
+        ReleaseMouseCapture();
+        e.Handled = true;
+
+        updateVisualState();
+      }
+    }
+    #endregion
+
+    #region Props
+    public Brush Brush {
+      get { return (Brush)GetValue(BrushProperty); }
+      set { SetValue(BrushProperty, value); }
+    }
+    public static readonly DependencyProperty BrushProperty =
+        DependencyProperty.Register("Brush", typeof(Brush), typeof(ConnectionPath), new FrameworkPropertyMetadata((Brush)null, FrameworkPropertyMetadataOptions.AffectsRender));
+    #endregion
+
+    protected override void OnRender(DrawingContext drawingContext) {
+      var figure = new PathFigure();
+      figure.IsClosed = false;
+      figure.IsFilled = false;
+      figure.StartPoint = new Point(0, 0);
+
+      const float margin = 10;
+      var height = RenderSize.Height;
+      var width = RenderSize.Width - 2 * margin;
+      Point[] points;
+      if (height > width) {
+        var displacement = height - width;
+        points = new Point[] { new Point(margin, 0),
+                               new Point(margin + width / 2, width / 2),
+                               new Point(margin + width / 2, width / 2 + displacement),
+                               new Point(RenderSize.Width - margin, RenderSize.Height),
+                               new Point(RenderSize.Width, RenderSize.Height) };
+      } else {
+        var displacement = width - height;
+        points = new Point[] { new Point(margin, 0),
+                               new Point(margin + height / 2, height / 2),
+                               new Point(margin + height / 2 + displacement, height / 2),
+                               new Point(RenderSize.Width - margin, RenderSize.Height),
+                               new Point(RenderSize.Width, RenderSize.Height) };
+      }
+
+      figure.Segments.Add(new PolyLineSegment(points, true));
+      var geo = new PathGeometry();
+      geo.Figures.Add(figure);
+
+      const float outlineSize = 10;
+      Pen shapeOutlinePen = new Pen(Brush, outlineSize);
+
+      Pen shapeOutlinePen2;
+      var BrushAsSolidColor = Brush as SolidColorBrush;
+      if (BrushAsSolidColor != null) {
+        shapeOutlinePen2 = new Pen(new SolidColorBrush(getMultipliedAlpha255(BrushAsSolidColor.Color)), outlineSize);
+      } else {
+        shapeOutlinePen2 = shapeOutlinePen;
+      }
+      drawingContext.DrawGeometry(null, shapeOutlinePen, geo);
+      drawingContext.DrawLine(shapeOutlinePen2, new Point(-margin, -2), new Point(0, -2));
+      drawingContext.DrawLine(shapeOutlinePen2, new Point(RenderSize.Width + margin, RenderSize.Height - 2), new Point(RenderSize.Width, RenderSize.Height - 2));
+    }
+
+    private Color getMultipliedAlpha255(Color input) {
+      var result = Color.Multiply(input, 1.5f);
+      result.A = 255;
+      return result;
+    }
+  }
+}
--- Controls/ConnectionsPanel.cs
+++ Controls/ConnectionsPanel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 using System.Windows.Shapes;
@@ -41,7 +42,7 @@ namespace NodeEditor.Controls {
         var connectionsItemsControl = ItemsControl.GetItemsOwner(this) as ItemsControl;
         var connectionsGenerator = connectionsItemsControl.ItemContainerGenerator;
         var nodesGenerator = nodesItemsControl.ItemContainerGenerator;
-        foreach(UIElement child in InternalChildren) {
+        foreach (UIElement child in InternalChildren) {
           if (child == null) { continue; }
 
           var connection = connectionsGenerator.ItemFromContainer(child) as Connection;
@@ -52,8 +53,6 @@ namespace NodeEditor.Controls {
             return finalSize;
           }
           var fromNodeOrigin = fromNodeContainer.TranslatePoint(new Point(0, 0), nodesItemsControl);
-          //var fromNodeSize = new Size(fromNodeContainer.RenderSize.Width * zoom,
-          //                            fromNodeContainer.RenderSize.Height * zoom);
           var fromNodeSize = nodesCartesianPanel.GetNodeSizeInfo(connection.FromNode).Size;
           fromNodeSize = new Size(fromNodeSize.Width * zoom,
                                   fromNodeSize.Height * zoom);
@@ -64,24 +63,16 @@ namespace NodeEditor.Controls {
             return finalSize;
           }
           var toNodeOrigin = toNodeContainer.TranslatePoint(new Point(0, 0), nodesItemsControl);
-          //var toNodeSize = new Size(toNodeContainer.RenderSize.Width * zoom,
-          //                          toNodeContainer.RenderSize.Height * zoom);
           var toNodeSize = nodesCartesianPanel.GetNodeSizeInfo(connection.ToNode).Size;
           toNodeSize = new Size(toNodeSize.Width * zoom,
                                 toNodeSize.Height * zoom);
 
           Point childOrigin;
           Size childSize;
-          //childOrigin = new Point(Math.Min(fromNodeOrigin.X, toNodeOrigin.X),
-          //                        Math.Min(fromNodeOrigin.Y, toNodeOrigin.Y));
-          //var childBottomRight = new Point(Math.Max(fromNodeOrigin.X + fromNodeSize.Width, toNodeOrigin.X + toNodeSize.Width),
-          //                                 Math.Max(fromNodeOrigin.Y + fromNodeSize.Height, toNodeOrigin.Y + toNodeSize.Height));
-          //childSize = new Size((childBottomRight.X - childOrigin.X) / zoom,
-          //                     (childBottomRight.Y - childOrigin.Y) / zoom);
 
           var verticalOutputOffset = 37.0 + 20.0 * connection.FromNode.GetOutputIndex(connection.FromNodeOutput);
           var verticalInputOffset = 37.0 + 20.0 * connection.ToNode.GetInputIndex(connection.ToNodeInput);
-          childOrigin = new Point(fromNodeOrigin.X + fromNodeSize.Width, 
+          childOrigin = new Point(fromNodeOrigin.X + fromNodeSize.Width,
                                   fromNodeOrigin.Y + verticalOutputOffset * zoom);
           childSize = new Size(toNodeOrigin.X - childOrigin.X,
                                toNodeOrigin.Y + verticalInputOffset * zoom - childOrigin.Y);
@@ -115,7 +106,7 @@ namespace NodeEditor.Controls {
       return NodeEditorControl.GetNodesCartesianPanel();
     }
 
-    private T GetVisualParent<T>(DependencyObject visual) where T: class {
+    private T GetVisualParent<T>(DependencyObject visual) where T : class {
       var curr = visual;
       while (curr != null) {
         var parent = VisualTreeHelper.GetParent(curr);
@@ -126,79 +117,6 @@ namespace NodeEditor.Controls {
         curr = parent;
       }
       return null;
-    }
-  }
-
-  class CustomPath: UIElement {
-    private DrawingVisual mVisual;
-    public CustomPath() {
-      DrawingVisual dv = new DrawingVisual();
-      
-
-      Effect = new DropShadowEffect();
-
-      mVisual = dv;
-      AddVisualChild(dv);
-    }
-
-    protected override void ArrangeCore(Rect finalRect) {
-      base.ArrangeCore(finalRect);
-
-      var figure = new PathFigure();
-      figure.IsClosed = false;
-      figure.IsFilled = false;
-      figure.StartPoint = new Point(0, 0);
-
-      var margin = 10;
-      var height = finalRect.Height;
-      var width = finalRect.Width - 2 * margin;
-      Point[] points;
-      if (height > width) {
-        var displacement = height - width;
-        points = new Point[] { new Point(margin, 0),
-                               new Point(margin + width / 2, width / 2),
-                               new Point(margin + width / 2, width / 2 + displacement),
-                               new Point(finalRect.Width - margin, finalRect.Height),
-                               new Point(finalRect.Width, finalRect.Height) };
-      } else {
-        var displacement = width - height;
-        points = new Point[] { new Point(margin, 0),
-                               new Point(margin + height / 2, height / 2),
-                               new Point(margin + height / 2 + displacement, height / 2),
-                               new Point(finalRect.Width - margin, finalRect.Height),
-                               new Point(finalRect.Width, finalRect.Height) };
-      }
-
-      //figure.Segments.Add(new PolyBezierSegment(points, true));
-      figure.Segments.Add(new PolyLineSegment(points, true));
-      var geo = new PathGeometry();
-      geo.Figures.Add(figure);
-
-      //Pen shapeOutlinePen = new Pen(new SolidColorBrush(Color.FromArgb(255, 114, 234, 114)), 12);
-      Pen shapeOutlinePen = new Pen(new SolidColorBrush(Color.FromArgb(255, 100, 200, 100)), 10);
-      Pen shapeInlinePen = new Pen(new SolidColorBrush(Color.FromArgb(255, 152, 251, 152)), 6);
-      Pen shapeOutlinePen2 = new Pen(new SolidColorBrush(Color.FromArgb(255, 0, 255, 0)), 10);
-      Pen shapeInlinePen2 = new Pen(new SolidColorBrush(Color.FromArgb(255, 0, 230, 0)), 6);
-      shapeOutlinePen.Freeze();
-      shapeInlinePen.Freeze();
-      shapeOutlinePen2.Freeze();
-      shapeInlinePen2.Freeze();
-      // Obtain a DrawingContext from 
-      // the DrawingGroup.
-      using (DrawingContext dc = mVisual.RenderOpen()) {
-        dc.DrawGeometry(Brushes.Green, shapeOutlinePen, geo);
-        dc.DrawGeometry(Brushes.Green, shapeInlinePen, geo);
-
-        dc.DrawLine(shapeOutlinePen2, new Point(-10, -2), new Point(0, -2));
-        dc.DrawLine(shapeInlinePen2, new Point(-10, -2), new Point(0, -2));
-        dc.DrawLine(shapeOutlinePen2, new Point(finalRect.Width + 10, finalRect.Height - 2), new Point(finalRect.Width, finalRect.Height - 2));
-        dc.DrawLine(shapeInlinePen2, new Point(finalRect.Width + 10, finalRect.Height - 2), new Point(finalRect.Width, finalRect.Height - 2));
-      }
-    }
-
-    protected override int VisualChildrenCount => 1;
-    protected override Visual GetVisualChild(int index) {
-      return mVisual;
     }
   }
 }
